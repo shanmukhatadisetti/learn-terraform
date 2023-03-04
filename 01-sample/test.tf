162356beb8294f66fefabe9ffc30b9a900685f21
shanmukh <shanmukhatadisetti@gmail.com>
2023-03-04 23:27:54 +0530

    out.tf

--- 01-sample/test.tf
+++ 01-sample/test.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "ec2" {
-  ami = "ami-0a017dceb274537d"
-  instance_type = "t3.micro"
+  ami                    = "ami-0a017dceb274537d"
+  instance_type          = "t3.micro"
   vpc_security_group_ids = ["sg-0fc69f59ac03ac238"]
   tags = {
     Name = "test"
