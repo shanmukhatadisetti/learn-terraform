28f58401b3bfbc27035851ce323c45758f26f408
shanmukh <shanmukhatadisetti@gmail.com>
2024-01-24 23:36:50 +0530

    Expense Script

--- 06-conditions/main.tf
+++ 06-conditions/main.tf
@@ -1,12 +1,12 @@
-#resource "aws_instance" "test" {
-#  ami           = "ami-03265a0778a880afb"
-#  instance_type = var.instance_type == null ? "t3.micro" : var.instance_type
-#
-#  tags = {
-#    Name = "test"
-#  }
-#}
-#
-#variable "instance_type" {
-#  default = null
-#}
+resource "aws_instance" "test" {
+  ami           = "ami-03265a0778a880afb"
+  instance_type = var.instance_type == null ? "t3.micro" : var.instance_type
+
+  tags = {
+    Name = "test"
+  }
+}
+
+variable "instance_type" {
+  default = null
+}
