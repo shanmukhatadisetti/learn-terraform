118c77f5d5ad64bda8bc49115a056165addec057
shanmukh <shanmukhatadisetti@gmail.com>
2024-01-26 23:33:45 +0530

    Expense Script

--- expense_version-1/data.tf
+++ expense_version-1/data.tf
@@ -1,15 +1,15 @@
-data "aws_ami" "ami" {
-  most_recent      = true
-  name_regex       = "Centos-8-DevOps-Practice"
-  owners           = ["973714476881"]
-}
-
-data "aws_route53_zone" "zone_id" {
-  name         = var.zone_id
-}
-
-data "aws_security_group" "sg" {
-  name = "allow-all"
-}
-
+#data "aws_ami" "ami" {
+#  most_recent      = true
+#  name_regex       = "Centos-8-DevOps-Practice"
+#  owners           = ["973714476881"]
+#}
+#
+#data "aws_route53_zone" "zone_id" {
+#  name         = var.zone_id
+#}
+#
+#data "aws_security_group" "sg" {
+#  name = "allow-all"
+#}
+#
 
