6a0196544a15575c23766160d7471b28bbd42be4
shanmukh <shanmukhatadisetti@gmail.com>
2024-01-28 23:21:32 +0530

    Expense Script

--- 02-vars/main.tf
+++ 02-vars/main.tf
@@ -1,26 +1,26 @@
-variable "fruits" {
-  default = "orange"
-}
-
-output "fruits" {
-  value = var.fruits
-}
-
-variable "fruits_list" {
-  default = ["apple","pineapple"]
-}
-
-output "fruits_list" {
-  value = var.fruits_list[0]
-}
-
-variable "fruits_mapp" {
-  default = {
-    grapes = 10
-    papaya = 20
-  }
-}
-
-output "fruits_price" {
-  value = "price of papaya = ${var.fruits_mapp["papaya"]}"
-}
+#variable "fruits" {
+#  default = "orange"
+#}
+#
+#output "fruits" {
+#  value = var.fruits
+#}
+#
+#variable "fruits_list" {
+#  default = ["apple","pineapple"]
+#}
+#
+#output "fruits_list" {
+#  value = var.fruits_list[0]
+#}
+#
+#variable "fruits_mapp" {
+#  default = {
+#    grapes = 10
+#    papaya = 20
+#  }
+#}
+#
+#output "fruits_price" {
+#  value = "price of papaya = ${var.fruits_mapp["papaya"]}"
+#}
