13054f82c2c33c0a6c8a547f8894762ae5e4508e
shanmukhasaitejatadisetti <shanmukhtadisetti9@gmail.com>
2025-03-18 16:30:46 +0000

    expense project1

--- expense-v1/main.tf
+++ expense-v1/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "frontend" {
   ami           = data.aws_ami.ami.image_id
   instance_type = "t3.micro"
-  vpc_security_group_ids = data.aws_security_group.sg.id
+  vpc_security_group_ids = [data.aws_security_group.sg.id]
 
   tags = {
     Name = "fromtend"
